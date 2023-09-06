5ea87f98645c610bae538e46b78398da4cf14377
ohseung1486 <ohseung1486@gmail.com>
2023-09-06 12:09:03 +0900

    집 컴퓨터에서 변경함

--- src/main/java/com/test/lesson01/GetMethodEx03.java
+++ src/main/java/com/test/lesson01/GetMethodEx03.java
@@ -15,6 +15,8 @@ public class GetMethodEx03 extends HttpServlet {
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		
+		// add comment
+		
 		// 한글 깨짐 방지
 //		response.setCharacterEncoding("utf-8");
 //		response.setContentType("text/plain");
